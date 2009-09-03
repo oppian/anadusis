b10287e81b8000a6ee82b106c3e867d63d39b53d
Stephen Hartley <steve@oppian.com>
2009-09-03 20:30:51 +0100

    Adding all the files for the first version

---
+++ settings.py
@@ -0,0 +1,145 @@
+# Django settings for server project.
+
+import os
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+SERVE_MEDIA = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+    ('Stephen Hartley', 'steve@oppian.com'),
+    ('Matthew Jacobi', 'matt@oppian.com'),
+)
+
+# Default admin account and pass
+# admin:merene54
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = os.path.join(PROJECT_ROOT, '.sqlite.db')             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/London'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-GB'
+
+SITE_ID = 1
+SITE_NAME = 'Lab'
+# work out the domain name
+# for local debugging on ports other than 80, this should be overridden in settings_local.py 
+import socket
+SITE_DOMAIN = socket.gethostname()
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media/')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/m/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'vn2iezxubn3g#-5d(lld-1_u9-as@@s7y!@r0-%r-r=)t1s+#o'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, "templates").replace('\\','/'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    
+    # 3rd party
+    
+    # Django Custom Management Command Extensions
+    # http://code.google.com/p/django-command-extensions/
+    'django_extensions',
+    
+    # inhouse
+    
+    # build tools
+    'build',
+)
+
+# fixture
+FIXTURE_DIRS = (
+    os.path.join(PROJECT_ROOT, "fixtures"),
+)
+
+
+# AWS
+AWS_ACCESS_KEY_ID = 'AKIAI5KRE2OCDICYSLFA'
+AWS_SECRET_ACCESS_KEY = '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'
+
+# build
+BUILD_VERSION = ('0', '1')
+BUILD_APPNAME = 'anadusis'
+AWS_BUILD_BUCKET_NAME = '%s-django' % BUILD_APPNAME
+
+BUILD_IGNORE = (
+    os.path.normpath(os.path.join(MEDIA_ROOT, 'upload')),
+)
+
+# Email
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'anadusis@oppian.com'
+EMAIL_HOST_PASSWORD = 'neetecel66'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# default from address for errors
+SERVER_EMAIL = '"%s" <anadusis@oppian.com>' % socket.gethostname()
+# default from address for normal email
+DEFAULT_FROM_EMAIL = 'anadusis@oppian.com'
+
+
+# local_settings.py can be used to override environment-specific settings
+# like database and email that differ between development and production.
+try:
+    from settings_local import *
+except ImportError:
+    pass
---
+++ settings_production.py
@@ -0,0 +1,10 @@
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'anadusis'              # Or path to database file if using sqlite3.
+DATABASE_USER = 'anadusis'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'bewmen69'         # Not used with sqlite3.
+DATABASE_HOST = 'localhost'             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
