b972574a50b11412f16b26ac573ce17e7f36cf50
Stephen Hartley <steve@oppian.com>
2010-05-25 23:11:08 +0100

    Removing account.context_processors.openid as it doesn't exist anymore

--- settings.py
+++ settings.py
@@ -124,7 +124,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     
     "notification.context_processors.notification",
     "announcements.context_processors.site_wide_announcements",
-    "account.context_processors.openid",
     "account.context_processors.account",
     "messages.context_processors.inbox",
     "friends_app.context_processors.invitations",
